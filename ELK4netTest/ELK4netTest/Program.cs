2a897111b8f4c85e94e29ade5ef7b1743c4ecf13
Edward YU <Edward4022@gmail.com>
2019-01-10 17:34:52 +0800

    destructors&tuple in C#7

--- ELK4netTest/ELK4netTest/Program.cs
+++ ELK4netTest/ELK4netTest/Program.cs
@@ -3,6 +3,7 @@ using log4net.Core;
 using log4net.Repository.Hierarchy;
 using Microsoft.ServiceBus.Messaging;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -29,165 +30,101 @@ namespace ELK4netTest
             LogHelper l = new LogHelper();
             l.Log(typeof(Test), LoggerLevel.Fatal, "ip", addr.Last().ToString(), "11111111111111111111111111111111111111111");
         }
+        
+        public void a()
+        {
+            var p = new Person("aaa");
+            p = null;
+        }
     }
 
     public class Program
     {
-        delegate void myFunction(string text);
-        static event myFunction readKeyEvent;
-        
-
-        private static int d(int x, int y)
-        {
-            return x + y;
-        }
-
-        private static int b(int x, int y)
-        {
-            return x - y;
-        }
-        class employee
-        {
-            private string m_date;
-            private string m_name;
-            private int m_score;
-            public employee(string cdate, string name, int score)
-            {
-                m_date = cdate;
-                m_name = name;
-                m_score = score;
-            }
-            public override string ToString()
-            {
-                return m_date + "," + m_name;
-            }
-        }
         static void Main(string[] args)
         {
-            List<employee> employees = new List<employee>
-            {
-                new employee("2012/1/18","龍龍一",100),
-                new employee("2012/1/18","龍龍二",100),
-                new employee("2012/1/18","龍龍三",100),
-                new employee("2012/1/19","龍龍一",100),
-                new employee("2012/1/19","龍龍二",100),
-                new employee("2012/1/19","龍龍三",100),
-                new employee("2012/1/18","龍龍三",100),
-                new employee("2012/1/18","龍龍一",100),
-            };
-            var q =
-from p in employees
-group p by p.ToString() into g
-where g.Count() > 1 //只顯示超過一次以上的
-select new
-{
-    g.Key,
-    count = g.Count()
-};
-            string stremp = "";
-            foreach (var x in q)
-            {
-                stremp += x.ToString() + Environment.NewLine;
-            }
-            Console.WriteLine(stremp);
+            var emp = getEmpInfo();
+            Console.WriteLine($"{emp.name}, {emp.age}");
 
-            using (StreamWriter sw  = new StreamWriter(@"d:\test.txt", false, new UTF8Encoding(false)))
-            {
-                sw.WriteLine("123");
-            }
-
-
-            Console.WriteLine("解法1");
-            DateTime cdate = new DateTime(2012, 1, 5);
-            for (int i = 1; i < 12; i++)
-            {
-                DateTime FirstDay = new DateTime(cdate.Year, cdate.Month, 1);//本月初1號
-                DateTime LastDay = new DateTime(cdate.AddMonths(1).Year, cdate.AddMonths(1).Month, 1).AddDays(-1);//下月初1號減一天=本月底
-                Console.WriteLine(FirstDay.ToString("yyyy/MM/dd"));
-                Console.WriteLine(LastDay.ToString("yyyy/MM/dd"));
-                cdate = cdate.AddMonths(1);//加一個月
-            }
-            Console.WriteLine("解法2");
-            DateTime startdate = new DateTime(2012, 1, 5);
-            for (int i = 1; i < 12; i++)
-            {
-                //本月初
-                DateTime TheMonthStart = new DateTime(startdate.Year, startdate.Month, 1);//本月初1號
-                Console.WriteLine(TheMonthStart.ToString("yyyy/MM/dd"));
-                //本月底
-                DateTime TheMonthEnd = new DateTime(startdate.Year, startdate.Month, DateTime.DaysInMonth(startdate.Year, startdate.Month));//本月初月底
-                Console.WriteLine(TheMonthEnd.ToString("yyyy/MM/dd"));
-                startdate = startdate.AddMonths(1);//加一個月
-            }
+            Console.ReadLine();
+            Person s = new Person("");
+            Console.WriteLine("Object Created ");
+            Console.WriteLine("Press enter to Destroy it");
+            Console.ReadLine();
+            s = null;
+            Person c = new Person("");
+            Console.WriteLine("Object Created ");
+            Console.WriteLine("Press enter to Destroy it");
+            Console.ReadLine();
+            c = null;
+            //GC.Collect();  
             Console.Read();
-
-            //foreach(var item in test_yield(9, 2))
-            //{
-            //    Console.WriteLine($"{item}");
-            //}
-            //Console.WriteLine();
-
-            //SpyPrinterToner toner = null;
-            //toner = new SpyPrinterToner();
-            //toner.OnSendMessage += new Action<string>(new MessageController().SendMessage);
-            //toner.CheckPrinterTonerIsLower();
-
-            //var a = 0;
-            //var str = "w";
-            //myFunction test = (string i) => { i += "s";  Console.WriteLine(i); };
-            //test(str);
-            //Console.WriteLine(str);
-            //Func<int, int, int> DoAdd = d;
-            //DoAdd += b;
-            //a = DoAdd(a, 2);
-            //Console.WriteLine(a);
-            //DoAdd doAdd = (int x, int y) =>
-            //{
-            //    return = x + y;
-            //};
         }
 
-        public static IEnumerable<int> test_yield(int max, int divide)
+        public static (string name, int age) getEmpInfo()
         {
-            for(int currentNum = 1; currentNum<= max; currentNum ++)
-            {
-                if (currentNum % divide == 1) continue;
-                yield return currentNum;
-            }
+            return ("123", 26);
         }
+    }
 
-        public class SpyPrinterToner
+
+
+    class Person
+    {
+        private static ConcurrentDictionary<int, string> names = new ConcurrentDictionary<int, string>();
+        private int id = 0;
+        
+
+        public Person(string name) => names.TryAdd(id, name); // constructors
+        ~Person() => Console.Write("123");              // destructors
+        public string Name
         {
-            public Action<string> OnSendMessage;
-
-            public void CheckPrinterTonerIsLower()
-            {
-                PhysicalPrinterAction action = new PhysicalPrinterAction();
-                int remainToner = action.SelectPrinterToner();
-                if (remainToner < 50)
-                {
-                    if (this.OnSendMessage != null)
-                    {
-                        this.OnSendMessage("Printer Name");
-                    }
-                }
-            }
+            get => names[id];                                 // getters
+            set => names[id] = value;                         // setters
         }
+    }
 
-        public class MessageController
+    public class Box
+    {
+        public int Width { get; private set; }
+        public int Hieght { get; private set; }
+        public int Long { get; private set; }
+
+        public Box(int w, int h, int l)
         {
-            public void SendMessage(string printerName)
-            {
-                //TODO: SendMessage
-            }
+            this.Width = w;
+            this.Hieght = h;
+            this.Long = l;
         }
 
-        public class PhysicalPrinterAction
+        public void Deconstruct(out int w, out int h)
         {
-            public int SelectPrinterToner()
-            {
-                return 40;
-            }
+            w = this.Width;
+            h = this.Hieght;
         }
+
+        public void Deconstruct(out int w, out int h,out int l)
+        {
+            w = this.Width;
+            h = this.Hieght;
+            l = this.Long;
+        }
+    }
+
+    public class Employee
+    {
+        private string _id;  // 屬性背後的實際欄位（backing field）
+
+        public string ID
+        {
+            get { return _id; }  // 屬性的讀取方法（getter）
+            set { _id = value; } // 屬性的設定方法（setter）
+        }
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        } //= "另壺沖"  // 編譯錯誤! 只有自動屬性才允許初始設定式。
     }
 }
